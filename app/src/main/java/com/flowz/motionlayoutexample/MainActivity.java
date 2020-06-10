24e01f4ea47b81f4acb43d23b7aa8b383d420ae5
FLOWZ <ketuimevbore@yahoo.com>
2020-06-10 11:23:35 -0700

    Initial commit

---
+++ app/src/main/java/com/flowz/motionlayoutexample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.flowz.motionlayoutexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.motion_layout);
+    }
+}
